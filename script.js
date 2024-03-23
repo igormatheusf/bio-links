5ef1c0ce1df2b60d55123032d8e29bcd687adca2
Igor <igorm.fonseca@hotmail.com>
2024-03-23 16:41:57 -0300

    updated avatar light

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute('src','./assets/avatar-light-2.png')
-    img.setAttribute("alt", "Foto de Igor Fonseca com óculos escuro, barba e camisa preta")
+    img.setAttribute('src','./assets/avatar-2.png')
+    img.setAttribute("alt", "Foto de Igor Fonseca com barba e camisa preta")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute('src', './assets/avatar-2.png')
